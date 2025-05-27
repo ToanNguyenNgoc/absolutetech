47ddd69d6330b0854df0477bd0158c8c01062166
Anh Duy <nguyenanhduyit01@gmail.com>
2025-05-27 22:14:53 +0700

    Add schema for module Job Number

---
+++ api/src/job-number/schemas/job-number.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document as MongooseDocument } from 'mongoose';
+
+export type JobNumberDocument = JobNumber & MongooseDocument;
+
+@Schema({ timestamps: true })
+export class JobNumber {
+  @Prop({ required: true, unique: true, index: true })
+  code: string; // Unique Job Number code (JN#)
+
+  @Prop({ required: true })
+  project: string; // Project name
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  assignedTo: mongoose.Types.ObjectId; // Person responsible (Line #1)
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  createdBy: mongoose.Types.ObjectId; // User who created the job
+
+  @Prop({ default: 'open', index: true })
+  status: string; // Job status: open, closed, approved
+}
+
+export const JobNumberSchema = SchemaFactory.createForClass(JobNumber);
+
+JobNumberSchema.virtual('documents', {
+  ref: 'DocumentEntity',
+  localField: '_id',
+  foreignField: 'jobNumber',
+  justOne: false,
+});
+
+JobNumberSchema.set('toObject', { virtuals: true });
+JobNumberSchema.set('toJSON', { virtuals: true });
