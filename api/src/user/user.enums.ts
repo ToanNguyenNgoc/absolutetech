70a01b0db25dcee1ab2f4344d65bb2dae979d1b5
Megabee Minh <megabee.hothanhminh@gmail.com>
2025-06-29 12:13:40 +0700

    add new

--- api/src/user/user.enums.ts
+++ api/src/user/user.enums.ts
@@ -1,17 +1,25 @@
 export enum Role {
   SUPER_ADMIN = 1,
   ADMINISTRATOR = 2,
-  TECHNICIAN = 3,
-  STOREMAN = 4,
+  ADMIN_SUPPORT = 3,
+  MANAGER = 4,
   SUPERVISOR = 5,
+  SITE_SUPERVISOR = 6,
+  STORE = 7,
+  WORKER = 8,
+  FREELANCE = 9,
 }
 
 export const RoleName: { [key in Role]: string } = {
   [Role.SUPER_ADMIN]: 'Super Admin',
   [Role.ADMINISTRATOR]: 'Administrator',
-  [Role.TECHNICIAN]: 'Technician',
-  [Role.STOREMAN]: 'Storeman',
+  [Role.ADMIN_SUPPORT]: 'Admin Support',
+  [Role.MANAGER]: 'MANAGER',
   [Role.SUPERVISOR]: 'Supervisor',
+  [Role.SITE_SUPERVISOR]: 'Site Supervisor',
+  [Role.STORE]: 'Store',
+  [Role.WORKER]: 'Worker',
+  [Role.FREELANCE]: 'Freelance',
 };
 
 export enum Gender {
