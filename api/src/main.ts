6207228c498954b1fc71fad6f892bd723499e133
Nguyen Toan <ngoctoan06011998>
2025-08-01 15:14:10 +0700

    update: transaction detail

--- api/src/main.ts
+++ api/src/main.ts
@@ -15,7 +15,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '20mb' }));
-  // app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
